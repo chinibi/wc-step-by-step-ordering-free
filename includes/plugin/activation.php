22d65ab0ede28e714ff62b4a19a18458bad8dc86
chinib <the1337cheat@gmail.com>
2017-05-16 11:00:12 -0700

    move plugin activation to its own file

---
+++ includes/plugin/activation.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *  Step-By-Step Plugin Activation Script
+ *
+ *
+ */
+
+if ( !post_exists( 'Step-By-Step Ordering' ) ) {
+  $page_data = array(
+    'post_status' => 'publish',
+    'post_type'   => 'page',
+    'post_author' => 1,
+    'post_name'   => 'ordering',
+    'post_title'  => 'Step-By-Step Ordering',
+    'comment_status' => 'closed',
+    'post_content'     => '[sbs_woocommerce_step_by_step_ordering]'
+  );
+
+  wp_insert_post( $page_data );
+}
